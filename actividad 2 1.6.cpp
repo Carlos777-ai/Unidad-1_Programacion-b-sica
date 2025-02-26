3c83aa89ed887f96301f7d6ddaa73cf138b69e75
Carlos777-ai <meme69kei@hotmail.com>
2025-02-26 17:58:20 -0600

    Add files via upload

---
+++ actividad 2 1.6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "string"
+
+using namespace std;
+
+int main()
+{
+    int A,B,C,D;
+    char oprimir_tecla_para_terminar;
+     //Mensaje de bienvenida
+    cout<<"Hola!Mi nombre el Miriam Este programar 1.6"<<endl;
+    cout<<"Escribe el primer valor A"<<endl;cin>>A;
+    cout<<"Escribe el primer valor B"<<endl;cin>>B;
+    cout<<"Escribe el primer valor C"<<endl;cin>>C;
+    cout<<"Escribe el primer valor D"<<endl;cin>>D;
+    cout<<"Los valores en orden inverso son."<<endl;
+    cout<<""<<D<<""<<C<<""<<""<<B<<""<<A<<endl;
+    cout<<D<<"--"<<C<<"--"<<B<<"--"<<A<<endl;
+}
